64b0b5884c5f6b56bc3eb994230bf020885d0259
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-04-21 11:57:38 +0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Welcome you on my page!</h1>
+      <h1>Welcome you on my page! </h1>
+      <h2>Personal portfolio</h2>
     </main>
   );
 }
