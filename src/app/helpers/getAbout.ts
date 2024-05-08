f8f97eed22a22655b3d69c72a953f7f95114298e
NurgaliyevS <nurgaliyev000@gmail.com>
2024-05-08 12:37:19 +0500

    getAbout add

---
+++ src/app/helpers/getAbout.ts
@@ -0,0 +1,6 @@
+export function getAbout() {
+    return `Frontend Developer with 3+ years of experience in web development. Proficient in building
+    scalable, performance-efficient, and user-centric web applications. Passionate about
+    solving human problems and contributing to product development with technical and
+    analytical skills.`
+}
