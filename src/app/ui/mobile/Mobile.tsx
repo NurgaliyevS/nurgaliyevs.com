a2e259218c49ae49cee02cdc79f8ed67c24686e8
NurgaliyevS <nurgaliyev000@gmail.com>
2024-05-08 12:20:27 +0500

    mobile upd

--- src/app/ui/mobile/Mobile.tsx
+++ src/app/ui/mobile/Mobile.tsx
@@ -24,7 +24,7 @@ function Mobile() {
 
                 <div className={styles["mt-2"]}>
                     <span className={`${styles["text-body-small"]} ${styles["break-words"]}`}>
-                        Kazakhstan
+                        Visa Required
                     </span>
                 </div>
             </section>
@@ -46,7 +46,7 @@ function Mobile() {
 
             <hr className={styles.line} />
 
-            <section className={`${styles.mobile} ${styles['pb-12']} ${styles['break-words']}`}>
+            <section className={`${styles.mobile} ${styles['pb-12']} ${styles['break-words']} ${styles["mr4"]}`}>
                 <h2 className={`${styles["text-body-medium"]} ${styles["text-color-text"]}  ${styles["pt-16"]} ${styles["mb-12"]} ${styles["text-heading-large"]} ${styles["text-heading-large"]}`}>Featured</h2>
                 <div className={`${styles["full-width"]} ${styles["full-height"]} ${styles["flex-column"]}`}>
                     <a className={`${styles["optional-action-target-wrappper"]}`} target="_blank" href={getResumeLink()}>
@@ -56,15 +56,22 @@ function Mobile() {
                     </a>
 
                     <a className={`${styles["optional-action-target-wrappper"]}`} target="_blank" href={getResumeLink()}>
-                        <div className={`${styles["responsive-container"]} ${styles["mr4"]}`}>
-                            <Image className={`${styles["responsive-image"]}`} width="320" height="195" src="/resume.jpeg" alt="Creator" />
+                        <div className={`${styles["responsive-container"]}`}>
+                            <Image className={`${styles["responsive-image"]}`} width="320" height="195" src="/resume.jpeg" alt="Resume" priority />
                         </div>
+
+                        <div className={`${styles["content-preview"]}`}>
+                            <div className={`${styles["display-flex"]} ${styles["flex-column"]} ${styles["text-heading-small"]} ${styles['break-words']}`}>
+                                <span>Front end developer CV</span>
+                            </div>
+                            <div className={`${styles["t-black--light"]} ${styles["text-body-xsmall"]} ${styles['break-words']}`}>
+                                <span>Google Docs</span>
+                            </div>
+                        </div>
+
                     </a>
                 </div>
-
             </section>
-
-            <hr className={styles.line} />
         </>
     );
 }
