b119ce5e233f86a9858590cd21fa56b91599b031
NurgaliyevS <nurgaliyev000@gmail.com>
2024-05-12 11:23:15 +0500

    get about upd

--- src/app/helpers/getAbout.ts
+++ src/app/helpers/getAbout.ts
@@ -1,5 +1,6 @@
 export function getAbout() {
-    return `Frontend Developer with 3+ years of experience in web development. 
-    Proficient in building scalable, performance-efficient, and user-centric web applications.
-    Passionate about solving human problems and contributing to product development with technical and analytical skills.`
+    return `• Front-end developer with over 3 years of experience in fintech, banking, insurance industries, working on client-server and web-based applications
+    • Increased sales by 30% through implementing a personalized advertising banner system for over 20,000 monthly active B2B users
+    • Reduced company hiring expenses by 40% through the implementation full-cycle recruitment service
+    `
 }
