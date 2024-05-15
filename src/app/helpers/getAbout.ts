b971b39beeceb06a202872054768f32968304d6a
NurgaliyevS <nurgaliyev000@gmail.com>
2024-05-15 09:10:20 +0500

    getAbout upd

--- src/app/helpers/getAbout.ts
+++ src/app/helpers/getAbout.ts
@@ -1,6 +1,9 @@
 export function getAbout() {
-    return `• Front-end developer with over 3 years of experience in fintech, banking, insurance industries, working on client-server and web-based applications
-    • Increased sales by 30% through implementing a personalized advertising banner system for over 20,000 monthly active B2B users
-    • Reduced company hiring expenses by 40% through the implementation full-cycle recruitment service
-    `
+    return `• Front-end developer with 4 years of experience in fintech, banking, and insurance industries
+    • Developed a personalized advertising banner system for over 20,000 monthly active B2B users
+    • Created an automated recruitment system to streamline paperwork processes
+    
+    Tech Stack: Typescript, Javascript, Node.js, Python, HTML5, CSS
+    Frontend technologies: React, Next.js, Angular, Redux, Redux Toolkit, RxJS, NgRx, PWA, SCSS
+    Tools: Jest, Testing Library, Gitlab CI/CD, Nginx, WebSocket, Git, Design Systems, Docker, Storybook, AWS, Playwright, Cypress`
 }
